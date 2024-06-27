254d5ad01b3e48ec581882515bd53831df4bcf5a
wholy <wholy@WHOLYOAK>
2024-06-26 19:30:02 -0500

    Agregar archivos de proyecto.

---
+++ Datos/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public static class Conexion
+    {
+        public static string cadena = "Server=wholyoak\\SQLEXPRESS; " +
+                                      "Initial Catalog=WICMonitoriaDBDev; " +
+                                      "User Id=wholyoak; Pwd=Vksur2013;" +
+                                      "trustservercertificate=True";
+    }
+}
